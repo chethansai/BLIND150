47792efeac835c4dd9d11aac4c318144ddeb9aa7
chethansaikrishna <chethansaikrishna@users.noreply.github.com>
2023-10-13 17:15:16 +0530

    Move forward and backward to put prefix and suffix product

---
+++ ARRAY & HASHING/Product of Array Except Self.py
@@ -0,0 +1,15 @@
+nums = [1, 2, 3, 4]
+prefix = 1
+suffix = 1
+ans = [1] * len(nums)
+for i in range(1, len(nums)):
+   ans[i-1] = prefix
+   prefix *= nums[i-1]
+
+for j in range(len(nums)-1,-1,-1):
+   ans[j] = suffix
+   suffix *= nums[j]
+
+
+
+print(ans)
