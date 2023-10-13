f6748703a8955297f2348256c164a91b5f66491c
chethansaikrishna <chethansaikrishna@users.noreply.github.com>
2023-10-13 10:27:43 +0530

    CONTAINS DUPLICATE FULLY SOLVED WITHOUT DEF BY USING BREAK AND FOR ELSE

---
+++ ARRAY & HASHING/Valid Anagram.py
@@ -0,0 +1,26 @@
+# s = "car"
+# t = "rat"
+# if(len(s) ==  len(t)):
+#    for i in s:
+#       if(i not in t):
+#          print("not an anagramm")
+#          break
+#    else:
+#       print("Anagram")
+#
+# else:
+#    print("not an anagram")
+def anagram():
+   s = "car"
+   t = "rat"
+   if(len(s) !=  len(t)):
+      return False
+   print("Ds")
+   ss,tt={},{}
+   for i in range(len(s)):
+      ss[s[i]] = 1 + ss.get(s[i],0)
+      tt[t[i]] = 1 + tt.get(t[i], 0)
+      print(i)
+   print(ss,tt)
+   return ss == tt
+print(anagram())
