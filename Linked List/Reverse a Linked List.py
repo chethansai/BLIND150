ecd20bae4d43bbb94378267764c8d2d024b20786
chethansaikrishna <chethansaikrishna@users.noreply.github.com>
2023-10-20 17:11:31 +0530

    HANDLE WHIEL CURRENT FIRST . PRESERVE NOT CURRETN BUT CURRENT.NEXT INTO TEMP. ACKNOWLEDGE NONE TO BE SET IN GIVEN HEAD'S NEXT

---
+++ Linked List/Reverse a Linked List.py
@@ -0,0 +1,65 @@
+class Node:
+   def __init__(self,data):
+      self.data = data
+      self.next = None
+
+class LinkedList:
+   def __init__(self):
+      self.head = None
+      self.tail = None
+
+   def append(self,data):
+      new_node = Node(data)
+
+      if( self.head is None):
+         self.head = new_node
+         self.tail = new_node
+
+      else:
+         self.tail.next= new_node
+         self.tail = new_node
+
+   def delete(self,data):
+
+      if(self.head is None):
+         return
+      if(self.head.data == data):
+         self.head = self.head.next
+         return
+      c = self.head
+      while(c.next):
+         if(c.next.data == data):
+            c.next =  c.next.next
+            return
+         c =c.next
+   def display(self):
+      c = self.head
+      while(c):
+         print(c.data)
+         c = c.next
+   def rev(self):
+      c = self.head
+      p = None
+      while(c):
+         t = c.next
+         c.next = p
+         p = c
+         c = t
+
+      self.head = p
+
+
+d = [1,2,3,4,5]
+
+ll = LinkedList()
+for i in d:
+   ll.append(i)
+
+
+ll.rev()
+ll.display()
+
+
+
+
+
