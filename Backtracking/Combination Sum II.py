8a1d1612dbf15439c764290fc4fc4b5a7117ba68
chethansai <chethansaikrishna@gmail.con>
2023-11-21 15:42:49 +0530

    SUBPROBLEM COMBINATION, SUM==TARGET. BT(i+1),pop,BT(i+1)

---
+++ Backtracking/Combination Sum II.py
@@ -0,0 +1,64 @@
+
+def answer():
+   ans = []
+   n = 5
+   k = 2
+   i = 1
+   tempans = []
+
+
+   def combinationk2n(i, tempans, ans, n, k):
+      if(sum(tempans) == 3):
+         ans.append(tempans.copy())
+         return
+      if(i>n or sum(tempans)>k):
+         return
+
+
+
+
+      tempans.append(i)
+
+      combinationk2n(i+1, tempans, ans, n, k)
+      tempans.pop()
+      combinationk2n(i+1, tempans, ans, n, k)
+
+   combinationk2n(i, tempans, ans, n, k)
+   return (ans)
+
+
+def answer1():
+   ans = []
+   n = 5
+   k = 2
+   i = 1
+   tempans = []
+
+
+   def combinationk2n(i, tempans, ans, n, k):
+      if(len(tempans) == k):
+         ans.append(tempans.copy())
+         return
+      if(i>n):
+         return
+
+
+
+
+
+      for i in range(i,n+1):
+         tempans.append(i)
+
+
+         combinationk2n(i+1, tempans, ans, n, k)
+         tempans.pop()
+
+
+   combinationk2n(i, tempans, ans, n, k)
+   return (ans)
+
+
+print(answer())
+
+print(answer1())
+
