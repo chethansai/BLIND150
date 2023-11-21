9b290ed51f9677e5dbb1e46cdfe918c9a051dd0f
chethansai <chethansaikrishna@gmail.con>
2023-11-21 15:33:00 +0530

    "DECISION TREE ANSWER : TAKE ONE, ANSWER ALSO : DONT TAKE TWO AND THREE
    ANSWER : TAKE ONE,TWO
    ANSWER ALSO : DONT TAKE THREE
    
    HENCE ANSWER : INCLUDE ELEMENT AND PROCEES - UNTIL END OF ARRAY.
                   EXCLUDE ELEMENT AND PROCEES -  UNTIL END OF ARRAY
    
    lists are mutable objects. When you do ans.append(tempans), you are appending a reference to the tempans
    2
    
    "

--- Backtracking/Subsets.py
+++ Backtracking/Subsets.py
@@ -81,7 +81,7 @@ def treemaze(root):
 
 
 
-def ans():
+def anssubsetswithoutrepetition():
    a = [1, 2, 3]
 
    #ans is global
@@ -108,13 +108,41 @@ def ans():
 
    subsets(i,a,ans,tempans)
    print(ans)
-tree = BinarySearchTree()
 
-tree.insert(1)
-tree.insert(2)
-tree.insert(4)
-tree.insert(3)
-tree.insert(5)
-print(treemaze(tree.root))
 
-print(ans())
+
+def anssubsetswithrepetition():
+   a =[1,2,3,2]
+   a.sort()
+   ans = []
+   tempans = []
+   i = 0
+   def subset(i, ans, tempans, a):
+
+      #INCLUDE ELEMENT AND PROCESS
+      if(i>=len(a)):
+         ans.append(tempans.copy())
+         return
+      tempans.append(a[i])
+
+      subset(i+1, ans, tempans, a )
+      r = len(a) - 1
+      #EXCLUDE ELEMENT AND PROCESS
+      tempans.pop()
+
+      #CHECK IF IT IS A REPEATED ELEMENT
+      #IMP FIRST CHECK INDEX AND INDEX ELEMENT EQUALS NEXT INDEX ELEMENT
+      while(i+1<len(a) and a[i]==a[i+1] ):
+         i = i + 1
+
+
+
+
+      subset(i+1, ans, tempans, a)
+   subset(i, ans, tempans, a)
+   print(ans)
+
+print(anssubsetswithoutrepetition())
+
+print(anssubsetswithrepetition())
+
