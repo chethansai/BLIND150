959936e830fb0f54fde4eabe9dc92cba6d7157ac
chethansai <chethansaikrishna@gmail.con>
2023-11-25 14:24:11 +0530

    CREATE ADJACENCY LIST. ANSWER = NO CYCLE = LEAF NODE EXISTS = GRAPH[LEAF_NODE] = [] AND G NOT ALREADY VISITED Q)UNDERSTAND WHAT IF VISIT WAS INITIALISED INSIDE DFS(). ANS)BECOMES NULL FOR EVERY RECURSION=>SO VISIT ADD=>LOOP=>REMOVE.

---
+++ Graphs/Course Schedule.py
@@ -0,0 +1,30 @@
+def dfs(g):
+   if g in visit:
+      return False
+   if len(graph[g]) == 0:
+      return True
+   visit.add(g)
+   for j in graph[g]:
+      if(not dfs(j)):
+         return False
+   visit.remove(g)
+   graph[g] = []
+   return True
+def ans():
+
+   for f, t in e:
+      if f not in graph:
+         graph[f] = []
+      if t not in graph:
+         graph[t] = []
+      graph[f].append(t)
+   print(graph)
+   for g in graph:
+      if(not dfs(g)):
+         return False
+   return True
+visit =set()
+e = [[0, 1], [0, 2], [1, 3], [1, 4],[3, 4]]
+#prepare graph
+graph = {}
+print(ans())
