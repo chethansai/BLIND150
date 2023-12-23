2c4d1bf2af0983cff055408be7314c3f7f26de60
Veena1003 <107175584+Veena1003@users.noreply.github.com>
2023-12-23 18:53:32 +0530

    COUNTER FOR LEFT.[ +/- LEFT,RIGHT]. ANYTIME < 0 FALSE. ATLAST == 0 TRUE. * SCENARIO SO TWO LEFT MIN,MAX.

---
+++ Greedy/Valid Parenthesis String.py
@@ -0,0 +1,31 @@
+def parenthesis(s):
+   print(s)
+   l_min = 0
+   l_max = 0
+
+   for i in s:
+      print(i)
+      if(i == '('):
+         l_min = l_min + 1
+         l_max = l_max + 1
+      if(i == '*'):
+         l_max = l_max + 1
+         l_min = l_min - 1
+      if(i == ')'):
+         l_min = l_min - 1
+         l_max = l_max - 1
+      if(l_min < 0):
+         l_min = 0
+      if(l_max < 0):
+
+         return False
+
+   if(l_min == 0):
+      return True
+   else:
+      return False
+
+def codejudge():
+   s = "(*)"
+   parenthesis(s)
+codejudge()
